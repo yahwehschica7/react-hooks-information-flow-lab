93cccfbcb78f037c34da44208e54ee4cd6a600e5
Jessica Nelson <yahwehschica@yahoo.com>
2022-02-01 11:41:20 -0500

    commit tree 343e3284ed836c84fdbc943f43f97954dc715874

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header"
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -10,16 +11,11 @@ function App() {
   }
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+    <div className={"App " + (isDarkMode ? "dark" : "light")}> 
+      <Header onDarkModeClick={handleDarkModeClick}/>
       <ShoppingList items={itemData} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+
+
+
+function Header({onDarkModeClick, isDarkMode}) {
+    
+    return (
+        <div>
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={onDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+                </button>
+            </header>
+        </div>
+        )
+    }
+    
+    export default Header  
+    
+
